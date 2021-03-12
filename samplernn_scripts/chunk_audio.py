359aba8c7a6b11532e5e6d3476f3743923826092
miranda kastemaa <miranda@foldplop.com>
2021-03-12 20:22:19 +0200

    reindent chunk_audio.py

--- samplernn_scripts/chunk_audio.py
+++ samplernn_scripts/chunk_audio.py
@@ -27,9 +27,9 @@ if not os.path.exists(output_dir):
 def is_silent(chunk, chunk_length):
     # if at least half of chunk is silence, mark as silent
     silences = silence.detect_silence(
-		chunk,
-		min_silence_len=int(chunk_length/2),
-		silence_thresh=-64)
+        chunk,
+        min_silence_len=int(chunk_length/2),
+        silence_thresh=-64)
     if silences:
         return True
     else:
@@ -53,33 +53,33 @@ num_silent = 0
 flag = 0 # Break loop once we reach the end of the audio
 for i in range(0, 8 * audio_len, chunk_length):
 
-	# At first, start is 0, end is the chunk_length
-	# Else, start=prvs end-overlap, end=start+chunk_length
-	if i == 0:
-		start = 0
-		end = chunk_length
-	else:
-		start = end - overlap
-		end = start + chunk_length
+    # At first, start is 0, end is the chunk_length
+    # Else, start=prvs end-overlap, end=start+chunk_length
+    if i == 0:
+        start = 0
+        end = chunk_length
+    else:
+        start = end - overlap
+        end = start + chunk_length
 
-	# Set flag to 1 if endtime exceeds length of file
-	if end >= audio_len:
-		end = audio_len
-		flag = 1
+    # Set flag to 1 if endtime exceeds length of file
+    if end >= audio_len:
+        end = audio_len
+        flag = 1
 
-	# Storing audio file from the defined start to end
-	chunk = audio[start:end]
-	if flag == 0:
-		cnt = cnt + 1
-		chunk_is_silent = is_silent(chunk, chunk_length)
-		if chunk_is_silent:
-			print('Chunk {} is silent, omitting it.'.format(cnt))
-			num_silent = num_silent + 1
-		else:
-			filename = input_filename + f'_chunk_{cnt}.wav'
-			chunk.export(os.path.join(output_dir, filename), format="wav")
-			print("Processing chunk " + str(cnt) + ". Start = "
-					+ str(start) + " end = " + str(end))
+    # Storing audio file from the defined start to end
+    chunk = audio[start:end]
+    if flag == 0:
+        cnt = cnt + 1
+        chunk_is_silent = is_silent(chunk, chunk_length)
+        if chunk_is_silent:
+            print('Chunk {} is silent, omitting it.'.format(cnt))
+            num_silent = num_silent + 1
+        else:
+            filename = input_filename + f'_chunk_{cnt}.wav'
+            chunk.export(os.path.join(output_dir, filename), format="wav")
+            print("Processing chunk " + str(cnt) + ". Start = "
+                    + str(start) + " end = " + str(end))
 
 print('\n')
 print("Finished chunking {}.".format(input_file))
